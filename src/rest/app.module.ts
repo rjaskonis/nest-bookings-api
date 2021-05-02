49e1bc74571faa2173b50c0a6dd5edc4ab7671ce
Renne Jaskonis <rjaskonis@gmail.com>
2021-05-02 19:54:35 -0300

    Added typeorm and professional module to app

--- src/rest/app.module.ts
+++ src/rest/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfessionalsModule } from './professional/professionals.module';
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forRoot(), ProfessionalsModule],
     controllers: [AppController],
     providers: [AppService],
 })
