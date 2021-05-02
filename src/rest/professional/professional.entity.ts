879970671b0fc60a4f40d24dfd17601b10056088
Renne Jaskonis <rjaskonis@gmail.com>
2021-05-02 19:51:49 -0300

    Created professional entity

---
+++ src/rest/professional/professional.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { ProfessionalAvailability } from '../professional-availability/professional-availability.entity';
+
+@Entity({ name: 'professionals' })
+export class Professional {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 150 })
+    name: string;
+
+    @Column({ type: 'varchar', length: 100 })
+    title: string;
+
+    @OneToMany(() => ProfessionalAvailability, (availability) => availability.professional, { cascade: true })
+    availabilities: ProfessionalAvailability[];
+}
