536e6f5fba140c7739b3e586b3c7cbfb9bf48a0a
Renne Jaskonis <rjaskonis@gmail.com>
2021-05-04 16:39:47 -0300

    Lib import syntax fix

--- test/integration/professionals.api.spec.ts
+++ test/integration/professionals.api.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { Connection, createConnection } from 'typeorm';
 import { AppModule } from '@rest/app.module';
 import { ProfessionalsController } from '@rest/professional/professionals.controller';
